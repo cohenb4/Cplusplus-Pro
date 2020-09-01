b262034d508c10e6fa86e667bbacc1e66040cdcf
cohenb4 <46035475+cohenb4@users.noreply.github.com>
2020-09-01 11:46:18 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <QCoreApplication>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+
+using namespace std;
+
+const int maxBarSize = 60;
+const int ciel(956/60);
+
+int minimum(int *v, int r);
+int maximum(int *v, int r);
+
+int random() {
+    int i;
+    cin >> i;
+        for (i = 0; i <= RAND_MAX; i++)
+        {
+            return i;
+        }
+
+    return i;
+}
+
+int main()
+{
+    return 0;
+}
